f84b7b7e1c924bcb6681c1c2723a65430c0058f7
mohdabdulirfan <irfanmohdabdul1@gmail.com>
2019-12-19 14:27:07 +0530

    New File Modified

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-console.log("Hello World !");
 const express = require('express')
 const app = express()
 const port =process.env.PORT||3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static("public"))
+app.get('/', (req, res) => res.sendFile(__dirname+"/public/html/new_1.html"))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- public/js/node_1.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const port =process.env.PORT||3000
-app.use(express.static("public"))
-app.get('/', (req, res) => res.sendFile(__dirname+"/public/html/new_1.html"))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
